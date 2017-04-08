86b39d752fb80b0474545fb7f7375d6a8c1b1b7d
Will Kaspar <Will@Aang.local>
2017-04-07 21:40:07 -0700

    adds pins route, app layout tabs are now links, correctly route.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :prints
 
+  get '/pins', controller: :pins, action: :index
+
   # Routes ONLY used for updating Print Order Status
   put '/prints/:id/bump_up', controller: :prints, action: :next_stage, as: 'bump_up'
   put '/prints/:id/bump_down', controller: :prints, action: :last_stage, as: 'bump_down'
