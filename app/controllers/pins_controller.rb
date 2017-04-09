1525404f474202b182f5a67d328b961e6ebcee58
Will Kaspar <Will@Aang.local>
2017-04-08 18:49:00 -0700

    pin index now shows list of pinball machines currently on the floor at freegoldwatch

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,7 +2,14 @@ class PinsController < ApplicationController
   include HTTParty
 
   def index
-    @response = HTTParty.get("https://pinballmap.com/api/v1/machines.json2")
+    get_pins
+  end
+
+  private
+
+  def get_pins
+    response = HTTParty.get('http://pinballmap.com/api/v1/locations/2405/machine_details.json', format: :json)
+    @pins = response['machines']
   end
 
 end
