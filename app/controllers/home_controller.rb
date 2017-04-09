344e9e212f2ae613bff6b039f2b0cd2831d6a00d
Will Kaspar <Will@Aang.local>
2017-04-09 15:21:10 -0700

    adds more assets, pin list created, basic layout created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,22 @@
 class HomeController < ApplicationController
 
   def index
+    get_pins
+    get_social
     render layout: "home"
+  end
+
+
+
+  private
+
+  def get_social
+
+  end
+
+  def get_pins
+    res = HTTParty.get('http://pinballmap.com/api/v1/locations/2405/machine_details.json', format: :json)
+    @pins = res['machines']
   end
 
 end
