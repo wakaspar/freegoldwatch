7cd66e7b3d9777b84cde6f8e7c8547ec402399c1
Will Kaspar <Will@aang.corp.fitbit.com>
2017-08-14 17:06:38 -0700

    attempting to move custom font up heroku's asset pipeline

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,6 +22,9 @@ Rails.application.configure do
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'stylesheets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf )
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
